32d1ea14a6d11868452282aa53569b569e64fe17
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2025-06-28 14:30:10 +0600

    Theme Updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#18069e',
+        secondary: '#e6aa05',
+      },
+    },
   },
   plugins: [],
 };
